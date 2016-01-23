15a26c820e4a05a72c93baf9b97f9822d43684a4
Colin Williams <colinwilliamsfoh@gmail.com>
2016-01-23 15:49:53 +0000

    added jumbotron widget template

---
+++ lib/widgets/jumbotron-widget/tpl/jumbotron-template.php
@@ -0,0 +1,6 @@
+<div class="fold home">
+  <div class="container">
+    <h1 class="center"><?php echo wp_kses_post($instance['text']) ?></h1>
+    <p class="center"><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+  </div>
+</div>
