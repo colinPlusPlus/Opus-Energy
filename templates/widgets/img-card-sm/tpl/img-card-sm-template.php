03ffef49243fcc456c7f592641fb2d15f0070abc
ColinPlusPlus <colinwilliamsfoh@gmail.com>
2016-02-02 17:27:52 -0500

    added img-card-sm

---
+++ templates/widgets/img-card-sm/tpl/img-card-sm-template.php
@@ -0,0 +1,17 @@
+<?php	$url = explode(':', $instance['url']); ?>
+<div class="img-wrap">
+	<img src="<?php echo wp_get_attachment_url($instance['background_image']); ?>" class="img-responsive" />
+  <div class="img-card-sm-wrap">
+  	<?php if ( $instance['header'] ): ?>
+    	<h2 class="text-uppercase text-center"><?php echo wp_kses_post($instance['header']) ?></h2>
+ 		<?php endif; ?>
+  	<?php if ( $instance['text_area'] ): ?>
+	  	<p><?php echo wp_kses_post($instance['text_area']) ?></p>
+  	<?php endif; ?>
+    <a class="btn <?php echo $instance['button'] ?>" 
+       href="<?php echo esc_url ( get_permalink ( $url[1] ) ) ?>" 
+       role="button">
+       <?php echo $instance['button_text'] ?>
+    </a>
+  </div>
+</div>
