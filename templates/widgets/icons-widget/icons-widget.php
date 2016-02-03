058b629e1085b3f17678601c24391ef7ef7b3d3e
Colin Williams <colinwilliamsfoh@gmail.com>
2016-02-03 00:45:44 +0000

    added business card widget

---
+++ templates/widgets/icons-widget/icons-widget.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+Widget Name: Opus Energy Icon Widget
+Description: An Opus Energy Widget
+Author: Colin Williams
+*/
+
+class Icon_Widget extends SiteOrigin_Widget {
+
+  function __construct() {
+    //Here you can do any preparation required before calling the parent constructor, such as including additional files or initializing variables.
+
+    //Call the parent constructor with the required arguments.
+    parent::__construct(
+      // The unique id for your widget.
+      'icon-widget',
+
+      // The name of the widget for display purposes.
+      __('Icon Widget', 'sage'),
+
+      // The $widget_options array, which is passed through to WP_Widget.
+      // It has a couple of extras like the optional help URL, which should link to your sites help or support page.
+      array(
+        'description' => __('An Opus Energy Widget', 'sage'),
+        'panels_groups' => array('opus'),
+      ),
+
+      //The $control_options array, which is passed through to WP_Widget
+      array(
+      ),
+
+      //The $form_options array, which describes the form fields used to configure SiteOrigin widgets. We'll explain these in more detail later.
+      array(
+       'icon_selection' => array(
+          'type' => 'select',
+          'prompt' => __( 'Choose an icon', 'sage' ),
+          'options' => array(
+            'icon-shop' => __( 'Shop Icon', 'sage' ),
+            'icon-question-mark' => __( 'Question Mark', 'sage' ),
+            'icon-document' => __( 'Document Icon', 'sage' ),
+            'icon-chat' => __( 'Chat Icon', 'sage' ),
+            'icon-conversation' => __( 'Conversation Icon', 'sage' ),
+            'icon-electricity' => __( 'Electricity Icon', 'sage' ),
+            'icon-gas' => __( 'Gas Icon', 'sage' ),
+            'icon-office' => __( 'Office Icon', 'sage' ),
+            'icon-meters' => __( 'Meters Icon', 'sage' ),
+            'icon-moving-truck' => __( 'Moving Truck', 'sage' ),
+            'icon-computer' => __( 'Computer Icon', 'sage' ),
+            'icon-pound' => __( 'British Pound', 'sage' ),
+            'icon-windmill' => __( 'Wind Energy', 'sage' ),
+            'icon-switch-to-us-icon' => __( 'Switch To Us', 'sage' )
+          )
+        ),
+        'button_selection_body' => array(
+          'type' => 'select',
+          'prompt' => __( 'Choose a Button Colour', 'sage' ),
+          'options' => array(
+            'btn-primary' => __('Pink', 'sage'),
+            'btn-default' => __('Green', 'sage')
+          )
+        ),
+       'button_text_body' => array(
+          'type' => 'text',
+          'prompt' => __( 'Button Text', 'sage' ),
+        ),  
+      ),
+    
+      //The $base_folder path string.
+      plugin_dir_path(__FILE__)
+    );
+  }
+
+  function get_template_name($instance) {
+    return 'icon-widget-template';
+  }
+
+}
+
+siteorigin_widget_register('icon-widget', __FILE__, 'Icon_Widget');
